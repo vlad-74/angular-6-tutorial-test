e92747aa3c92487a5188d1c1bfe5d5c77826faa4
vlad-74 <kurguzov74@gmail.com>
2020-05-03 11:16:38 +0300

    2 files modified, 1 added

--- src/my-form/my-form.module.ts
+++ src/my-form/my-form.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MyFormComponent } from './my-form/my-form.component';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { ReactiveFormsModule }   from '@angular/forms'; 
+import { FormsModule }   from '@angular/forms';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserModule,
+    FormsModule,
+    
   ],
   declarations: [MyFormComponent],
   exports: [MyFormComponent],
--- src/my-form/my-form/my-form.component.ts
+++ src/my-form/my-form/my-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import * as moment from 'moment/moment';
 
 @Component({
   selector: 'app-my-form',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyFormComponent implements OnInit {
 
+  public assessmentForm: FormGroup;
+  public fields: Array<any> = [];
+  public fromDate = new Date().getTime();
+
   constructor() { }
 
   ngOnInit() {
+    this.initFields();
   }
 
+  initFields() {
+    const monthEarlier = moment().subtract(1, 'months').format('YYYY-MM-DD');
+
+    this.fields = [
+      {
+        control: new FormControl(monthEarlier, []),
+        fieldName: 'from',
+        name: 'from',
+        controlType: 'date',
+        label: 'c',
+        placeholder: 'Выберите дату'
+      },
+      {
+        control: new FormControl(new Date(this.fromDate), []),
+        fieldName: 'to',
+        name: 'to',
+        controlType: 'date',
+        label: 'по',
+        placeholder: 'Выберите дату',
+        classList: ['periodEnd']
+      },
+    ];
+
+    this.assessmentForm = new FormGroup(this.fields.reduce((group, field) => {
+      group[field.fieldName] = field.control;
+      return group;
+    }, {}));
+
+  }
 }
