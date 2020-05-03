4c9160b462d1300ab0ae0ebbd14d44289759a6f6
vlad-74 <kurguzov74@gmail.com>
2020-05-03 10:35:21 +0300

    1 file modified, 2 added

--- src/my-form/my-form.module.ts
+++ src/my-form/my-form.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MyFormComponent } from './my-form/my-form.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [MyFormComponent]
 })
 
 export class MyFormModule { }
