ed17bf679595cecbf8249082225e9b07b999192c
vlad-74 <kurguzov74@gmail.com>
2020-05-04 09:49:27 +0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'; // <- добавлено
 
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
@@ -11,8 +12,8 @@ import { BookItemComponent } from "./book-item/book-item.component";
 import { AppService } from './app.service';
 import { FormComponent } from './form/form.component';
 import { ReactiveFormsModule }   from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { HttpService } from './http.service';
+import { AuthInterceptor, ParamInterceptor } from './api.interceptor';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, ReactiveFormsModule, HttpClientModule],
@@ -27,7 +28,17 @@ import { HttpService } from './http.service';
   ],
   entryComponents: [BookItemComponent],
   bootstrap: [AppComponent],
-  providers: [AppService, HttpService]
+  providers: [AppService, HttpService,
+  HttpService, {
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptor,
+    multi: true // option. This is required and tells Angular that HTTP_INTERCEPTORS is an array of values, rather than a single value.
+  }, {
+    provide: HTTP_INTERCEPTORS,
+    useClass: ParamInterceptor,
+    multi: true
+  }
+  ]
 })
 
 export class AppModule {}
