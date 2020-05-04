38d61e493953eb73a623fc3fc93d54852a67dfe1
vlad-74 <kurguzov74@gmail.com>
2020-05-04 20:32:52 +0300

    1 file modified, 2 added

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,15 +4,17 @@ import { FormsModule } from "@angular/forms";
 import { ReactiveFormsModule }   from '@angular/forms';
 import { HomeComponent } from './home.component';
 import { Routes, RouterModule } from '@angular/router';
+import { ItemsComponent } from './items/items.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const itemRoutes: Routes = [{ path: 'items', component: ItemsComponent}];
+const routes: Routes = [{ path: '', component: HomeComponent, children: itemRoutes }];
 
 @NgModule({
   imports: [
     CommonModule, FormsModule, ReactiveFormsModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, ItemsComponent],
   exports: [RouterModule]
 })
 
