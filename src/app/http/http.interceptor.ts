f1a4d6e451aaef0659804df743c2c41390054d77
vlad-74 <kurguzov74@gmail.com>
2020-05-04 10:45:08 +0300

    1 file modified

--- src/app/http/http.interceptor.ts
+++ src/app/http/http.interceptor.ts
@@ -5,54 +5,42 @@ import { tap, catchError } from 'rxjs/operators';
 
 @Injectable()
 export class ParamInterceptor implements HttpInterceptor {
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (req.url.includes('jsonplaceholder.typicode.com')) {
+        const paramReq = req.clone({ params: req.params.set('userId', '7') });
 
-      if (req.url.includes('jsonplaceholder.typicode.com')) {
-          const paramReq = req.clone({ params: req.params.set('userId', '7') });
-
-          return next.handle(paramReq).pipe(
-            tap(evt => {
-
-              if (evt instanceof HttpResponse) {
-                if(evt.body && evt.status === 200){ console.log('evt.body', evt.body) }
+        return next.handle(paramReq).pipe(
+          tap(evt => {
+            if (evt instanceof HttpResponse) {
+              if(evt.body && evt.status === 200){ console.log('evt.body', evt.body) }
+            }
+          }),
+          catchError((err: any) => {
+              if(err instanceof HttpErrorResponse) {
+                  try {
+                    console.log('try - err instanceof HttpErrorResponse')
+                  } catch(e) {
+                    console.log('catch - err instanceof HttpErrorResponse', e)
+                  }
               }
-            }),
-            catchError((err: any) => {
-                if(err instanceof HttpErrorResponse) {
-                    try {
-                        console.log('try - err instanceof HttpErrorResponse')
-                    } catch(e) {
-                      console.log('catch - err instanceof HttpErrorResponse', e)
-                    }
-                }
-                return of(err);
-            }));
-      } else {
-          console.log('req - ', req);
-          return next.handle(req);
-      }
-
+              return of(err);
+          }));
+    } else {
+        console.log('req - ', req);
+        return next.handle(req);
     }
+  }
 }
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+  // constructor(private auth: AuthService) {}
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    // const authHeader = this.auth.getToken();
+    const authHeader = 'Bearer 1A2b3C4d5E6f7G8h9IAgBKClD';
 
-    // constructor(private auth: AuthService) {}
-
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
-        // const authHeader = this.auth.getToken();
-        const authHeader = 'Bearer 1A2b3C4d5E6f7G8h9IAgBKClD';
-
-        const authReq = req.clone({
-            headers: req.headers.set(
-                'Authorization',
-               authHeader
-            )
-        });
-         console.log('authReq - ', authReq);
-        return next.handle(authReq);
-
-    }
+    const authReq = req.clone({ headers: req.headers.set( 'Authorization', authHeader ) });
+    console.log('authReq - ', authReq);
+    return next.handle(authReq);
+  }
 }
