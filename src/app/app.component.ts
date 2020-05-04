796090b6cc232e1888d2ff07947dd887d5613217
vlad-74 <kurguzov74@gmail.com>
2020-05-04 19:37:03 +0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'my-app',
@@ -6,4 +8,11 @@ import { Component} from '@angular/core';
   styleUrls: [ './app.component.css' ],
   // encapsulation: ViewEncapsulation.None // https://dzone.com/articles/what-is-viewencapsulation-in-angular
 })
-export class AppComponent {}
+export class AppComponent {
+
+  constructor(private router: Router){
+    router.events
+      .pipe( filter(event => event instanceof NavigationEnd) )
+      .subscribe( (event: NavigationEnd) => {console.log(event.url);} );
+  }    
+}
