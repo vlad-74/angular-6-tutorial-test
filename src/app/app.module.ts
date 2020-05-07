12da0e645f4fde05d49db6dfcfec87491d3d4787
vlad-74 <kurguzov74@gmail.com>
2020-05-07 16:08:48 +0300

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,34 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule } from '@angular/material';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import {
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+  MatCardModule
+} from "@angular/material";
 
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'; 
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
 
 import { BookItemComponent } from "./components/book-item/book-item.component";
 
-import { IfHasAccessService } from './services/if-has-access.service';
-import { HttpService } from './services/http/http.service';
-import { AuthInterceptor, ParamInterceptor } from './services/http/http.interceptor';
+import { IfHasAccessService } from "./services/if-has-access.service";
+import { HttpService } from "./services/http/http.service";
+import {
+  AuthInterceptor,
+  ParamInterceptor
+} from "./services/http/http.interceptor";
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   imports: [
-    BrowserModule, 
+    BrowserModule,
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
@@ -29,23 +39,22 @@ import { AppRoutingModule } from './app-routing.module';
     MatListModule,
     MatCardModule
   ],
-  declarations: [
-    AppComponent,
-    BookItemComponent
-  ],
+  declarations: [AppComponent, BookItemComponent],
   entryComponents: [BookItemComponent],
   bootstrap: [AppComponent],
-  providers: [IfHasAccessService,
-  HttpService, {
-    provide: HTTP_INTERCEPTORS,
-    useClass: AuthInterceptor,
-    multi: true // option. This is required and tells Angular that HTTP_INTERCEPTORS is an array of values, rather than a single value.
-  }, {
-    provide: HTTP_INTERCEPTORS,
-    useClass: ParamInterceptor,
-    multi: true
-  }
+  providers: [
+    IfHasAccessService,
+    HttpService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true // option. This is required and tells Angular that HTTP_INTERCEPTORS is an array of values, rather than a single value.
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ParamInterceptor,
+      multi: true
+    }
   ]
 })
-
 export class AppModule {}
